4f0422f24f7ef1749bfc63a7b8d85f1e2e4ae735
DennisKimani <dennisngugi.dk@gmail.com>
2017-07-18 10:40:46 +0300

    added a migration for due_dates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718071515) do
+ActiveRecord::Schema.define(version: 20170718073633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170718071515) do
     t.boolean "done"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "due_date"
   end
 
 end
